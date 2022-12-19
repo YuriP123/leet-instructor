4151fbae48d1287e74a66e55c3e24bdd55d39323
YuriP123 <miguel.pasamonte2@gmail.com>
2022-12-19 07:30:39 -0800

    feat modal form input

---
+++ src/pages/Components/TaskModal.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from "react";
+import { Box,Button,Modal,ModalOverlay,ModalContent,ModalHeader,ModalFooter,ModalBody,ModalCloseButton, useDisclosure,Input, Slider, SliderTrack,SliderFilledTrack,SliderThumb,SliderMark,} from "@chakra-ui/react";
+import {CirclePicker} from 'react-color';
+
+function useLocalState(key){
+  const [state, setState] = useState(()=>{
+      if (typeof window !== 'undefined') {
+        localStorage.getItem(key)
+      }
+    }
+  )
+
+  useEffect(()=>{
+    window.localStorage.setItem(key,state)
+  },[key,state])
+
+  return [state,setState]
+}
+
+
+export default function TaskModal(){
+    const [task,setTask] = useLocalState('task')
+    const [color,setColor] = useLocalState('color')
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    function handleNameChange(event){
+      setTask(event.target.value)
+    }
+    function handleColorChange(color){
+      setColor(color.hex)
+    }
+
+    return (
+      
+      <div>
+        <Button onClick={onOpen}>Add New Task</Button>
+        <Modal isOpen={isOpen} onClose={onClose} >
+          <ModalOverlay />
+          <ModalContent mt="400px" >
+            <ModalHeader>Task Info</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Input placeholder="Task Name" mb="20px" value={task} onChange={handleNameChange}></Input>
+              <h3>Difficulty</h3>
+              <SliderComponent/>
+              <Box mt="50px" >
+                <CirclePicker value={color} onChangeComplete={handleColorChange}/>
+              </Box>
+            </ModalBody>
+  
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} onClick={onClose} >
+                Add Task
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </div>
+    )
+  }
+  function SliderComponent(){
+    const [sliderValue, setSliderValue] = useLocalState('slider',0)
+    const labelStyles = {
+      mt: '2',
+      fontSize: 'sm',
+      pr: '2px'
+    }
+
+    function handleChange(val){
+      setSliderValue(val)
+    }
+  
+    return (
+      <Box>
+        <Slider aria-label='slider-ex-6' defaultValue={3} min={1} max={5} step={1} onChange={val => handleChange(val)}>
+          <SliderMark value={1} {...labelStyles}>
+            1
+          </SliderMark>
+          <SliderMark value={2} {...labelStyles}>
+            2
+          </SliderMark>
+          <SliderMark value={3} {...labelStyles}>
+            3
+          </SliderMark>
+          <SliderMark value={4} {...labelStyles}>
+            4
+          </SliderMark>
+          <SliderMark value={5} {...labelStyles}>
+            5
+          </SliderMark>
+          <SliderTrack>
+            <SliderFilledTrack />
+          </SliderTrack>
+          <SliderThumb />
+        </Slider>
+      </Box>
+    )
+  }
+
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,7 @@
 import React from "react";
-import { Box,Container, Button,Modal,ModalOverlay,ModalContent,ModalHeader,ModalFooter,ModalBody,ModalCloseButton,Lorem, useDisclosure} from "@chakra-ui/react";
+import { Box,Container, Button} from "@chakra-ui/react";
 import styles from "../Styles/Home.module.scss"
-import Taskframe from "./Components/Taskframe"
-
-function TaskOverlay(){
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  return (
-    <>
-      <Button onClick={onOpen}>Open Modal</Button>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Lorem count={2} />
-          </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant='ghost'>Secondary Action</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
-  )
-}
+import TaskModal from "./Components/TaskModal"
 
 export default function Home(){
 
@@ -40,7 +13,7 @@ export default function Home(){
                 <Button colorScheme='yellow'>Home</Button>
                 <Button colorScheme='yellow'>History</Button>
                 <Button colorScheme='yellow'>Graphs</Button>
-                <TaskOverlay/>
+                <TaskModal/>
             </Box>
             <Box className={styles.stage}color="white">
             </Box>
