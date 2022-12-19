f36c3a23e2f8d4e21e703c86487490040e317ef8
YuriP123 <miguel.pasamonte2@gmail.com>
2022-12-19 03:00:53 -0800

    added modal

--- src/pages/Components/Taskframe.js
+++ src/pages/Components/Taskframe.js
@@ -1,16 +1,16 @@
 import { Container, IconButton} from "@chakra-ui/react";
 import { AddIcon } from '@chakra-ui/icons'
-import React from "react";
+import {React, useState} from "react";
 import styles from "../../Styles/Healthbars.module.scss"
+import Healthbar from "./Healthbar"
 
-export default function Healthbars(){
+export default function Taskframe(){
+    const [health, setHealth] = useState(800)
     return(
         <Container className={styles.taskFrame} pt="50px">
             <div>
                 <h1>Title</h1>
-                <div className="HealthbarOverlay">
-                    <img src="healthbar_frame.png" alt="healthbar"></img>
-                </div>
+                <Healthbar current={health}/>
             </div>
             <IconButton
             mt="35px"
@@ -18,6 +18,7 @@ export default function Healthbars(){
             colorScheme='green'
             aria-label='Search database'
             icon={<AddIcon />}
+            //onClick={addTaskDetails}
             />
         </Container>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,48 @@
 import React from "react";
-import {Container, Box} from "@chakra-ui/react"
+import { Box,Container, Button,Modal,ModalOverlay,ModalContent,ModalHeader,ModalFooter,ModalBody,ModalCloseButton,Lorem, useDisclosure} from "@chakra-ui/react";
 import styles from "../Styles/Home.module.scss"
-import Healthbars from "./Components/Healthbars"
+import Taskframe from "./Components/Taskframe"
+
+function TaskOverlay(){
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Lorem count={2} />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
 
 export default function Home(){
+
+    //const taskList = taskArray.map(() => <Taskframe/>)
+
     return(
         <Container maxW='2xl' centerContent>
             <Box boxShadow='md' color='black' className={styles.frame} mt={"200px"} width="1000px" borderRadius="md">
-                <ul className="frame tw-text-none">
-                    <li><a href="Home">Home</a></li>
-                    <li><a href="History">History</a></li>
-                    <li><a href="Graphs">Graphs</a></li>
-                    <li><a href="NewTask">+ Add New Task</a></li>
-                </ul>
+                <Button colorScheme='yellow'>Home</Button>
+                <Button colorScheme='yellow'>History</Button>
+                <Button colorScheme='yellow'>Graphs</Button>
+                <TaskOverlay/>
             </Box>
             <Box className={styles.stage}color="white">
-                <Healthbars/>
-                <Healthbars/>
-                <Healthbars/>
             </Box>
         </Container>
     )
